534022b059e4b37742cd3a35524ed4cde5d709ad
IosifG <77391293+IosifG1@users.noreply.github.com>
2024-04-28 08:40:27 +0300

    added buttons

--- Assets/ClassSelectionAvailableExample.cs
+++ Assets/ClassSelectionAvailableExample.cs
@@ -9,22 +9,24 @@ public class ClassSelectionAvailableExample : MonoBehaviour
     private uint _selectedClass = 0;
     private bool _update = false;
     public StreetViewAPI _streetViewAPI;
+
     private bool test = false;
 
     [SerializeField] ERPFlashController3D _flashController;
     [SerializeField] Dictionary<int, Renderer> _selectedObjects;
     void Start()
     {
+
         //attach to class selection available event
         BCIManager.Instance.ClassSelectionAvailable += OnClassSelectionAvailable;
 
         //get selected objects
         _selectedObjects = new Dictionary<int, Renderer>();
         List<ERPFlashObject3D> applicationObjects = _flashController.ApplicationObjects;
-        foreach(ERPFlashObject3D applicationObject in applicationObjects)
+        foreach (ERPFlashObject3D applicationObject in applicationObjects)
         {
             Renderer[] renderers = applicationObject.GameObject.GetComponentsInChildren<Renderer>();
-            foreach(Renderer renderer in renderers)
+            foreach (Renderer renderer in renderers)
             {
                 if (renderer.name.Equals("selected"))
                     _selectedObjects.Add(applicationObject.ClassId, renderer);
@@ -32,7 +34,7 @@ public class ClassSelectionAvailableExample : MonoBehaviour
         }
 
         // disable/hide selected objects by default
-        foreach(KeyValuePair<int, Renderer> kvp in _selectedObjects)
+        foreach (KeyValuePair<int, Renderer> kvp in _selectedObjects)
             kvp.Value.gameObject.SetActive(false);
     }
 
@@ -51,7 +53,7 @@ public class ClassSelectionAvailableExample : MonoBehaviour
     void Update()
     {
         //TODO ADD YOUR CODE HERE
-        if(_update && test)
+        if (_update && test)
         {
             foreach (KeyValuePair<int, Renderer> kvp in _selectedObjects)
                 kvp.Value.gameObject.SetActive(false);
@@ -59,6 +61,8 @@ public class ClassSelectionAvailableExample : MonoBehaviour
             if (_selectedClass > 0)
             {
                 _selectedObjects[(int)_selectedClass].gameObject.SetActive(true);
+
+                bool recorded=false;
                 switch (_selectedClass)
                 {
                     // North
@@ -77,11 +81,22 @@ public class ClassSelectionAvailableExample : MonoBehaviour
                     case 4:
                         _streetViewAPI.MoveSouth();
                         break;
+                    case 5:
+                        Speech2Text.instance.StartRecording();
+                        recorded=true;
+                        break;
+
+                    case 6:
+                    if(recorded){
+                        Speech2Text.instance.SendRecording();
+                        recorded=false;
+                    }
+                        break;
                 }
             }
 
             _update = false;
-        } 
+        }
     }
 
     /// <summary>
@@ -92,9 +107,9 @@ public class ClassSelectionAvailableExample : MonoBehaviour
     private void OnClassSelectionAvailable(object sender, EventArgs e)
     {
         ClassSelectionAvailableEventArgs ea = (ClassSelectionAvailableEventArgs)e;
-       _selectedClass = ea.Class;
+        _selectedClass = ea.Class;
         _update = true;
-       // Debug.Log(string.Format("Selected class: {0}", ea.Class));
+        // Debug.Log(string.Format("Selected class: {0}", ea.Class));
 
     }
 }
